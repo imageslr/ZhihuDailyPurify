a6a680e6bb29b70200af1bf99fd5329d236bb617
ZhuKaihao <zhukaihaorj@163.com>
2017-05-13 22:07:34 +0800

    关于界面

--- app/src/main/java/com/zhukaihao/zhihudailypurify/ui/activity/LicenseActivity.java
+++ app/src/main/java/com/zhukaihao/zhihudailypurify/ui/activity/LicenseActivity.java
@@ -1,11 +1,7 @@
 package com.zhukaihao.zhihudailypurify.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.zhukaihao.zhihudailypurify.R;
 
@@ -16,12 +12,28 @@ import com.zhukaihao.zhihudailypurify.R;
 public class LicenseActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        layoutResID = R.layout.activity_apache_license;
         super.onCreate(savedInstanceState);
 
+        TextView textView = (TextView) findViewById(R.id.license_text);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(getString(R.string.licences_header)).append("\n");
+
+        String[] basedOnProjects = getResources().getStringArray(R.array.apache_licensed_projects);
+
+        for (String str : basedOnProjects) {
+            sb.append("• ").append(str).append("\n");
+        }
+
+        sb.append("\n").append(getString(R.string.licenses_subheader));
+        sb.append("\n\n").append(getString(R.string.apache_license));
+        textView.setText(sb.toString());
+
         // 添加license内容
-        ((FrameLayout) findViewById(R.id.fragment_frame))
-                .addView((RelativeLayout) LayoutInflater.from(this)
-                        .inflate(R.layout.apache_license, null, false)
-                        .findViewById(R.id.apache_license_layout));
+//        ((FrameLayout) findViewById(R.id.fragment_frame))
+//                .addView((RelativeLayout) LayoutInflater.from(this)
+//                        .inflate(R.layout.apache_license, null, false)
+//                        .findViewById(R.id.apache_license_layout));
     }
 }
